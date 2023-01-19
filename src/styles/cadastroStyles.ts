52b8047856069a525c5ce5c2d2f73d101b28684a
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-01-19 00:09:35 -0300

    Ajustando identação

--- src/styles/cadastroStyles.ts
+++ src/styles/cadastroStyles.ts
@@ -1,174 +1,182 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 750px;
-    height: 700px;
-    position: absolute;
-    transform: translate(-50%, 0);
-    left: 50%;
-    top: 120px;
-    background-color: var(--dark);
-    animation: overlayAnimation 0.2s ease;
-    border-radius: 10px;
-    z-index: 32;
+  width: 750px;
+  height: 700px;
+  position: absolute;
+  transform: translate(-50%, 0);
+  left: 50%;
+  top: 120px;
+  background-color: var(--dark);
+  animation: overlayAnimation 0.2s ease;
+  border-radius: 10px;
+  z-index: 32;
 `;
 export const Overlay = styled.div`
-    width: 100%;
-    height: 100%;
-    background-color: rgb(0 0 0/0.3);
-    top: 0px;
-    position: absolute;
-    z-index: 30;
-    animation: name duration timing-function delay iteration-count direction fill-mode;
-    animation: overlayAnimation 0.8s ease;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(0 0 0/0.3);
+  top: 0px;
+  position: absolute;
+  z-index: 30;
+  animation: name duration timing-function delay iteration-count direction
+    fill-mode;
+  animation: overlayAnimation 0.8s ease;
 `;
 
 export const Header = styled.div`
+  margin: 10px 0px;
+
+  svg {
+    width: 100%;
+    height: 70px;
+  }
+
+  p {
+    color: rgba(235, 235, 235, 0.7);
     margin: 10px 0px;
-
-    svg {
-        width: 100%;
-        height: 70px;
-    }
-
-    p {
-        color: rgba(235,235,235,0.7);
-        margin: 10px 0px;
-        text-align: center;
-    }
+    text-align: center;
+  }
 `;
 export const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    height: 305px;
+  display: flex;
+  flex-direction: column;
+  height: 305px;
 
-    input {
-        outline: none;
-        border: none;
-        font-family: 'Poppins', sans-serif;
-        padding: 15px;
-        display: block;
-        width: 80%;
-        background-color: transparent;
-        border: 1px solid var(--grey);
-        margin: 0 auto;
-        font-size: 16px;
-        color: #fff;
-        transition: 0.4s all;
-    }
+  input {
+    outline: none;
+    border: none;
+    font-family: "Poppins", sans-serif;
+    padding: 15px;
+    display: block;
+    width: 80%;
+    background-color: transparent;
+    border: 1px solid var(--grey);
+    margin: 0 auto;
+    font-size: 16px;
+    color: #fff;
+    transition: 0.4s all;
+  }
 
-    .icon {
-        color: #fff;
-        position: absolute;
-        right: 95px;
-        top: 17px;
-        width: 24px;
-        height: 24px;
-        transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
-    }
+  .icon {
+    color: #fff;
+    position: absolute;
+    right: 95px;
+    top: 17px;
+    width: 24px;
+    height: 24px;
+    transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
 
-    label {
-        color: rgba(235,235,235,0.7);
-        position: absolute;
-        font-size: 16px;
-        top: 16px;
-        left: 93px;
-        pointer-events: none;
-        transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
-    }
+  label {
+    color: rgba(235, 235, 235, 0.7);
+    position: absolute;
+    font-size: 16px;
+    top: 16px;
+    left: 93px;
+    pointer-events: none;
+    transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
 
-    input:focus {
-        border-color: var(--blue);
-    }
+  input:focus {
+    border-color: var(--blue);
+  }
 
-    input:focus ~ label, 
-    input:not(:placeholder-shown) ~ label {
-        transform: translateX(6px) translateY(-25px);
-        font-size: 14px;
-        padding: 0 10px;
-        background: var(--dark);
-        color: var(--blue);
-    }
+  input:focus ~ label,
+  input:not(:placeholder-shown) ~ label {
+    transform: translateX(6px) translateY(-25px);
+    font-size: 14px;
+    padding: 0 10px;
+    background: var(--dark);
+    color: var(--blue);
+  }
 
-    input:focus ~ .icon, 
-    input:not(:placeholder-shown) ~ .icon {
-        color: var(--blue);
-    }
+  input:focus ~ .icon,
+  input:not(:placeholder-shown) ~ .icon {
+    color: var(--blue);
+  }
 
-    input:invalid {
-        border-color: red;
-    }
+  input:not(:placeholder-shown) {
+    border-color: var(--blue);
+  }
+
+  input:invalid {
+    border-color: red;
+  }
+
+  input:invalid ~ .icon,
+  input:invalid ~ label {
+    color: red;
+  }
 `;
 
 export const Email = styled.div`
-    position: relative;
-    width: 100%;
-    top: 40px;
-    display: flex;
+  position: relative;
+  width: 100%;
+  top: 40px;
+  display: flex;
 `;
 
 export const Nome = styled.div`
-    position: relative;
-    width: 100%;
-    top: 70px;
-    display: flex;
+  position: relative;
+  width: 100%;
+  top: 70px;
+  display: flex;
 `;
 
 export const Senha = styled.div`
-    position: relative;
-    width: 100%;
-    top: 100px;
-    display: flex;
+  position: relative;
+  width: 100%;
+  top: 100px;
+  display: flex;
 `;
 
-
 export const Footer = styled.div`
-    p {
-        color: rgba(235,235,235,0.7);
-        text-align: center;
-    }
+  p {
+    color: rgba(235, 235, 235, 0.7);
+    text-align: center;
+  }
 
-    a {
-        color: var(--blue);
-        text-align: center;
-    }
+  a {
+    font-size: 16px;
+    color: var(--blue);
+    text-align: center;
+  }
 `;
 
 export const Button = styled.div`
-    position: relative;
-    top: 120px;
+  position: relative;
+  top: 120px;
 
-    button {
-        outline: none;
-        display: block;
-        margin: 15px auto;
-        width: 50%;
-        border: none;
-        background-color: transparent;
-        color: var(--grey);
-        font-family: 'Poppins', sans-serif;
-        font-size: 14px;
-        border: 1px solid var(--grey);
-        padding: 15px;
-        transition: 0.4s all;
-        cursor: pointer;
-    }
+  button {
+    outline: none;
+    display: block;
+    margin: 15px auto;
+    width: 50%;
+    border: none;
+    background-color: transparent;
+    color: var(--grey);
+    font-family: "Poppins", sans-serif;
+    font-size: 14px;
+    border: 1px solid var(--grey);
+    padding: 15px;
+    transition: 0.4s all;
+    cursor: pointer;
+  }
 
-    button:hover {
-        box-shadow: inset 0 -3.95em 0 0 var(--blue);
-        border: 1px solid var(--blue);
-    }
+  button:hover {
+    box-shadow: inset 0 -3.95em 0 0 var(--blue);
+    border: 1px solid var(--blue);
+  }
 `;
 
 export const Text = styled.div`
-    position: absolute;
-    width: 100%;
-    bottom: 40px;
+  position: absolute;
+  width: 100%;
+  bottom: 40px;
 
-    .senha {
-        display: block;
-        margin: 10px auto;
-    }
+  .senha {
+    display: block;
+    margin: 10px auto;
+  }
 `;
-
- 
--- src/styles/headerStyles.ts
+++ src/styles/headerStyles.ts
@@ -1,146 +1,145 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.header`
-    background-color: var(--dark);
-    height: 80px;
-    display: flex;
-    align-items: center;
+  background-color: var(--dark);
+  height: 80px;
+  display: flex;
+  align-items: center;
 `;
 
 export const Logo = styled.div`
-    padding-left: 2%;
+  padding-left: 2%;
 
-    p {
-        text-align: center;
-        color: var(--grey);
-    }
+  p {
+    text-align: center;
+    color: var(--grey);
+  }
 `;
 
 export const HeadNav = styled.div`
-    position: absolute;
-    right: 7%;
-    cursor: pointer;
-    transition: 0.4s all;
-    display: none;
-    visibility: hidden;
+  position: absolute;
+  right: 7%;
+  cursor: pointer;
+  transition: 0.4s all;
+  display: none;
+  visibility: hidden;
 
-    div {
-        width: 20px;
-        height: 2px;
-        background: var(--grey);
-        margin: 5px;
-        transition: all 0.2s ease;
-    }
+  div {
+    width: 20px;
+    height: 2px;
+    background: var(--grey);
+    margin: 5px;
+    transition: all 0.2s ease;
+  }
 
-    &.active .line-1 {
-        transform: rotate(-45deg) translate(-5px, 5px);
-    }
-    &.active .line-2 {
-        opacity: 0;
-    }
-    &.active .line-3 {
-        transform: rotate(45deg) translate(-5px, -5px);
-    }
+  &.active .line-1 {
+    transform: rotate(-45deg) translate(-5px, 5px);
+  }
+  &.active .line-2 {
+    opacity: 0;
+  }
+  &.active .line-3 {
+    transform: rotate(45deg) translate(-5px, -5px);
+  }
 
-    @media (max-width: 768px) {
-        visibility: visible;
-        display: inline;
-    }
-
+  @media (max-width: 768px) {
+    visibility: visible;
+    display: inline;
+  }
 `;
 
 export const Ul = styled.ul`
-    display: flex;
-    margin: 0 auto;
-    list-style: none;
-    justify-content: right;
-    align-items: center;
+  display: flex;
+  margin: 0 auto;
+  list-style: none;
+  justify-content: right;
+  align-items: center;
 
-    a {
-        text-decoration: none;
-        transition: 0.4s all;
-        color: var(--grey);
-        font-size: 18px;
+  a {
+    text-decoration: none;
+    transition: 0.4s all;
+    color: var(--grey);
+    font-size: 18px;
+  }
+
+  .li a:after {
+    display: block;
+    content: "";
+    border-bottom: solid 2px var(--grey);
+    margin-right: 2px;
+    transform: scaleX(0);
+    transition: transform 320ms ease-in-out;
+  }
+
+  .li:hover a:after {
+    transform: scaleX(1);
+  }
+
+  .li {
+    margin: 0px 30px;
+    font-weight: 500;
+    font-size: 18px;
+  }
+
+  @media (max-width: 768px) {
+    position: fixed;
+    top: 80px;
+    right: 0;
+    width: 0px;
+    height: 92vh;
+    flex-direction: column;
+    background-color: var(--dark);
+    justify-content: space-around;
+    transition: all 0.4s ease-in;
+    z-index: 999;
+
+    li {
+      width: 0;
+      opacity: 0;
+      margin: 0;
     }
 
-    .li a:after {
-        display:block;
-        content: '';
-        border-bottom: solid 2px var(--grey);
-        margin-right: 2px;
-        transform: scaleX(0);
-        transition: transform 320ms ease-in-out;
+    &.active {
+      width: 210px;
     }
-
-    .li:hover a:after {
-        transform: scaleX(1);
-    }
-
-    .li {
-        margin: 0px 30px;
-        font-weight: 500;
-        font-size: 18px;
-    }
-
-    @media (max-width: 768px) {
-        position: fixed;
-        top: 80px;
-        right: 0;
-        width: 0px;
-        height: 92vh;
-        flex-direction: column;
-        background-color: var(--dark);
-        justify-content: space-around;
-        transition: all 0.4s ease-in;
-        z-index: 999;
-        
-        li {
-            width: 0;
-            opacity: 0;
-            margin: 0;
-        }
-
-        &.active {
-            width: 210px;
-        }
-    }
+  }
 `;
 
 export const Button = styled.div`
-    width: 15%;
+  width: 15%;
 
-    button {
-        outline: none;
-        width: 120px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border: none;
-        background-color: transparent;
-        color: var(--grey);
-        font-family: 'Poppins', sans-serif;
-        margin: 0px auto;
-        border: 1px solid var(--grey);
-        padding: 10px;
-        transition: 0.4s all;
-        cursor: pointer;
-    }
+  button {
+    outline: none;
+    width: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    background-color: transparent;
+    color: var(--grey);
+    font-family: "Poppins", sans-serif;
+    margin: 0px auto;
+    border: 1px solid var(--grey);
+    padding: 10px;
+    transition: 0.4s all;
+    cursor: pointer;
+  }
 
-    .icon {
-        font-size: 18px;
-        margin-right: 5px;
-    }
+  .icon {
+    font-size: 18px;
+    margin-right: 5px;
+  }
 
-    button:hover {
-        box-shadow: inset 0 -3.95em 0 0 var(--blue);
-        border: 1px solid var(--blue);
-    }
+  button:hover {
+    box-shadow: inset 0 -3.95em 0 0 var(--blue);
+    border: 1px solid var(--blue);
+  }
 
-    @media (max-width: 1220px) {
-        width: 25%;
-    }
+  @media (max-width: 1220px) {
+    width: 25%;
+  }
 
-    @media (max-width: 768px) {
-        width: 45%;
-    }
+  @media (max-width: 768px) {
+    width: 45%;
+  }
 `;
