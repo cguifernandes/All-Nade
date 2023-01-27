a29615b0c2f39f84eb2c9be5611f8d46fa1d4770
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-01-27 11:42:51 -0300

    Loading main

--- src/components/Main/main.tsx
+++ src/components/Main/main.tsx
@@ -7,41 +7,46 @@ import LoadingMain from "../Loading/mainLoading";
 
 const Main = () => {
     const [movies, setMovies] = useState<typeMovies[]>();
+    const [isLoading, setIsLoading] = useState(true);
     const key = process.env.NEXT_PUBLIC_API_KEY;
     const urlImg = process.env.NEXT_PUBLIC_API_IMG;
 
     useEffect(() => {
-        api.get(`/movie/top_rated?api_key=${key}&language=pt-BR&page=1&region=BR`).then(({data}) => {
-            setMovies(data.results);
+        api.get(`/movie/top_rated?api_key=${key}&language=pt-BR&page=1&region=BR`)
+        .then(res => {
+            setMovies(res.data.results);
+            setIsLoading(false);
         });
     });
+
+    console.log(isLoading)
 
     return (
       <Container>
         {
-        movies?.length == 0 ?
-        movies?.map((movie) => {
+            isLoading ?
+            movies?.map((movie) => {
                 return <LoadingMain key={movie.vote_count}></LoadingMain>;
             })
-        :
-        movies?.map((movie) => {
-            return (
-                <>
-                    <Card key={movie.id}>
-                        <Img>
-                            <h3>{movie.title }</h3>
-                            <img src={urlImg + movie.poster_path}/>
-                        </Img>
-                        <Text>
-                            <div className="vote">
-                                <p className="vote">{movie.vote_average}</p>
-                            </div>
-                            <p>{movie.overview.slice(0, 139)}...</p>
-                        </Text>
-                    </Card>
-                </>
-            )
-        })
+            :
+            movies?.map((movie) => {
+                return (
+                    <>
+                        <Card key={movie.id}>
+                            <Img>
+                                <h3>{movie.title }</h3>
+                                <img src={urlImg + movie.poster_path}/>
+                            </Img>
+                            <Text>
+                                <div className="vote">
+                                    <p className="vote">{movie.vote_average}</p>
+                                </div>
+                                <p>{movie.overview.slice(0, 139)}...</p>
+                            </Text>
+                        </Card>
+                    </>
+                )
+            })
         }
       </Container>
     );
